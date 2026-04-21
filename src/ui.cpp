b8ca091224c95e0bc1d29612a133f218b87d159b
Jack Humbert <jack.humb@gmail.com>
2026-04-21 17:10:57 -0400

    Refine Rajdhani clock layout

--- src/ui.cpp
+++ src/ui.cpp
@@ -11,6 +11,7 @@
 namespace {
 lv_obj_t *sTimeLabel = nullptr;
 lv_obj_t *sMetaLabel = nullptr;
+lv_obj_t *sWeatherRow = nullptr;
 lv_obj_t *sWeatherIcon = nullptr;
 lv_obj_t *sWeatherLabel = nullptr;
 lv_obj_t *sCurrentTempLabel = nullptr;
@@ -19,16 +20,40 @@ lv_obj_t *sStatusLabel = nullptr;
 char sLastTimeText[16] = {};
 char sLastMetaText[32] = {};
 
-const lv_font_t *time_font()
+const lv_font_t *rajdhani_regular_font(int32_t size, const lv_font_t *fallback)
 {
 #if LV_USE_TINY_TTF
-    lv_font_t *font = RAJDHANI_REGULAR(64);
+    lv_font_t *font = RAJDHANI_REGULAR(size);
     if (font != nullptr) {
         return font;
     }
 #endif
 
-    return &lv_font_montserrat_48;
+    return fallback;
+}
+
+const lv_font_t *rajdhani_medium_font(int32_t size, const lv_font_t *fallback)
+{
+#if LV_USE_TINY_TTF
+    lv_font_t *font = RAJDHANI_MEDIUM(size);
+    if (font != nullptr) {
+        return font;
+    }
+#endif
+
+    return fallback;
+}
+
+const lv_font_t *rajdhani_semibold_font(int32_t size, const lv_font_t *fallback)
+{
+#if LV_USE_TINY_TTF
+    lv_font_t *font = RAJDHANI_SEMIBOLD(size);
+    if (font != nullptr) {
+        return font;
+    }
+#endif
+
+    return fallback;
 }
 }
 
@@ -39,50 +64,63 @@ void ui_init()
     lv_obj_set_style_text_color(screen, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
 
     sTimeLabel = lv_label_create(screen);
-    lv_obj_set_width(sTimeLabel, 320);
+    lv_obj_set_width(sTimeLabel, 430);
     lv_obj_set_style_text_align(sTimeLabel, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_set_style_text_font(sTimeLabel, time_font(), 0);
-    lv_obj_set_style_text_letter_space(sTimeLabel, 1, 0);
+    lv_obj_set_style_text_font(sTimeLabel, rajdhani_regular_font(192, &lv_font_montserrat_48), 0);
+    lv_obj_set_style_text_letter_space(sTimeLabel, -2, 0);
     lv_label_set_text(sTimeLabel, "--:--");
-    lv_obj_align(sTimeLabel, LV_ALIGN_TOP_MID, 0, 28);
+    lv_obj_align(sTimeLabel, LV_ALIGN_TOP_MID, 0, -8);
 
     sMetaLabel = lv_label_create(screen);
-    lv_obj_set_width(sMetaLabel, 260);
+    lv_obj_set_width(sMetaLabel, 360);
     lv_obj_set_style_text_align(sMetaLabel, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_set_style_text_font(sMetaLabel, &lv_font_montserrat_20, 0);
+    lv_obj_set_style_text_font(sMetaLabel, rajdhani_medium_font(40, &lv_font_montserrat_20), 0);
+    lv_obj_set_style_text_letter_space(sMetaLabel, 1, 0);
     lv_label_set_text(sMetaLabel, "Waiting for time");
-    lv_obj_align_to(sMetaLabel, sTimeLabel, LV_ALIGN_OUT_BOTTOM_MID, 0, 4);
+    lv_obj_align_to(sMetaLabel, sTimeLabel, LV_ALIGN_OUT_BOTTOM_MID, 0, -8);
 
-    sWeatherIcon = weather_icons_create(screen);
-    lv_obj_align(sWeatherIcon, LV_ALIGN_CENTER, 0, -16);
+    sWeatherRow = lv_obj_create(screen);
+    lv_obj_remove_style_all(sWeatherRow);
+    lv_obj_set_size(sWeatherRow, 340, 104);
+    lv_obj_set_layout(sWeatherRow, LV_LAYOUT_FLEX);
+    lv_obj_set_flex_flow(sWeatherRow, LV_FLEX_FLOW_ROW);
+    lv_obj_set_flex_align(sWeatherRow, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+    lv_obj_set_style_pad_column(sWeatherRow, 14, 0);
+    lv_obj_set_style_pad_all(sWeatherRow, 0, 0);
+    lv_obj_align_to(sWeatherRow, sMetaLabel, LV_ALIGN_OUT_BOTTOM_MID, 0, 2);
+
+    sWeatherIcon = weather_icons_create(sWeatherRow);
 
     sWeatherLabel = lv_label_create(screen);
-    lv_obj_set_width(sWeatherLabel, 220);
+    lv_obj_set_width(sWeatherLabel, 360);
     lv_obj_set_style_text_align(sWeatherLabel, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_set_style_text_font(sWeatherLabel, &lv_font_montserrat_14, 0);
+    lv_obj_set_style_text_font(sWeatherLabel, rajdhani_semibold_font(28, &lv_font_montserrat_28), 0);
+    lv_obj_set_style_text_letter_space(sWeatherLabel, 1, 0);
     lv_label_set_text(sWeatherLabel, "WEATHER");
-    lv_obj_align_to(sWeatherLabel, sWeatherIcon, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+    lv_obj_align_to(sWeatherLabel, sWeatherRow, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
 
-    sCurrentTempLabel = lv_label_create(screen);
-    lv_obj_set_width(sCurrentTempLabel, 220);
-    lv_obj_set_style_text_align(sCurrentTempLabel, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_set_style_text_font(sCurrentTempLabel, &lv_font_montserrat_48, 0);
+    sCurrentTempLabel = lv_label_create(sWeatherRow);
+    lv_obj_set_width(sCurrentTempLabel, 190);
+    lv_obj_set_style_text_align(sCurrentTempLabel, LV_TEXT_ALIGN_LEFT, 0);
+    lv_obj_set_style_text_font(sCurrentTempLabel, rajdhani_semibold_font(96, &lv_font_montserrat_48), 0);
+    lv_obj_set_style_text_letter_space(sCurrentTempLabel, -1, 0);
     lv_label_set_text(sCurrentTempLabel, "--");
-    lv_obj_align_to(sCurrentTempLabel, sWeatherLabel, LV_ALIGN_OUT_BOTTOM_MID, 0, 8);
 
     sRangeLabel = lv_label_create(screen);
-    lv_obj_set_width(sRangeLabel, 220);
+    lv_obj_set_width(sRangeLabel, 360);
     lv_obj_set_style_text_align(sRangeLabel, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_set_style_text_font(sRangeLabel, &lv_font_montserrat_20, 0);
+    lv_obj_set_style_text_font(sRangeLabel, rajdhani_medium_font(40, &lv_font_montserrat_20), 0);
+    lv_obj_set_style_text_letter_space(sRangeLabel, 1, 0);
     lv_label_set_text(sRangeLabel, "H --  L --");
-    lv_obj_align_to(sRangeLabel, sCurrentTempLabel, LV_ALIGN_OUT_BOTTOM_MID, 0, 10);
+    lv_obj_align_to(sRangeLabel, sWeatherLabel, LV_ALIGN_OUT_BOTTOM_MID, 0, -2);
 
     sStatusLabel = lv_label_create(screen);
-    lv_obj_set_width(sStatusLabel, 320);
+    lv_obj_set_width(sStatusLabel, 400);
     lv_label_set_long_mode(sStatusLabel, LV_LABEL_LONG_SCROLL_CIRCULAR);
-    lv_obj_set_style_text_font(sStatusLabel, &lv_font_montserrat_14, 0);
+    lv_obj_set_style_text_font(sStatusLabel, rajdhani_medium_font(28, &lv_font_montserrat_28), 0);
+    lv_obj_set_style_text_letter_space(sStatusLabel, 1, 0);
     lv_label_set_text(sStatusLabel, "Booting");
-    lv_obj_align(sStatusLabel, LV_ALIGN_BOTTOM_MID, 0, -22);
+    lv_obj_align(sStatusLabel, LV_ALIGN_BOTTOM_MID, 0, -10);
 }
 
 void ui_set_time(const ClockSnapshot &snapshot)
